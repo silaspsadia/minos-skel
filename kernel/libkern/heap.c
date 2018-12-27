b1351211a7a8c2d0f7cdf9094338b00f6b5a1830
isajisai <silaspsadia@gmail.com>
2018-12-26 16:33:24 -0800

    clone from minos repo

--- kernel/libkern/heap.c
+++ kernel/libkern/heap.c
@@ -14,124 +14,30 @@
 #define MIN_BLOCK_SIZE 	1024
 #define HEAP_START_SIZE (8192 / sizeof(header_t))
 
-static header_t *wilderness;
-static header_t *_flist_head;
 static header_t base;
 
+// Put your setup routines here (init data structures etc.)
 void kheap_init(void)
 {
-	wilderness = HEAP_VIRT_ADDR_START;
-	alloc_pages((virtual_addr *) wilderness, 2);
-	_flist_head = wilderness;
-	_flist_head->size = HEAP_START_SIZE;
-	_flist_head->prev = _flist_head;
-	_flist_head->next = _flist_head;
-	wilderness += 8192;
-	printf("[Mem ] Heap initialized at %x\n", _flist_head);
 }
 
+// This is the main function for allocating kernel memory
 void *kmalloc(size_t nbytes)
 {
-	printf("[DEBUG] kmalloc\n");
-	header_t *cur, *prev;
-	size_t nunits;	
-
-	/* Can't allocate 0 or negative */
-	if (nbytes <= 0) 
-		return NULL;
-
-	nunits = div_ceil(nbytes + sizeof(header_t), sizeof(header_t));
-
-	for (cur = _flist_head; ; cur = cur->next) {
-		if (cur->size >= nunits) {
-			if (cur->size == nunits) {
-				cur->prev->next = cur->next;
-			} else {
-				cur->size -= nunits;
-				cur += cur->size;
-				cur->size = nunits;
-			}	
-			print_flist_head();	
-			return (header_t *) (cur + 1);
-		} 
-		if (cur == _flist_head) {
-			if ((cur = acquire_more_heap(nunits)) == NULL) {
-				return NULL;
-			}
-			cur = cur->prev;
-		}
-	}
 }
 
+// This is a complement to kmalloc and should be reentrant
 void kfree(void *ap)
 {
-	printf("[DEBUG] kfree\n");
-	header_t *bp, *p, *p_next;
-	bp = (header_t *) ap - 1;
-	for (p = _flist_head, p_next = _flist_head->next; ; p = p_next, p_next = p_next->next)
-		if ((p < bp && p_next > bp) || (p->next == _flist_head))
-			break;
-
-	bp->prev = p;
-	bp->next = p_next;
-	p_next->prev = bp;
-	p->next = bp;
-	
-	if (bp + bp->size == p_next) {
-		printf("upper boundary merge\n");
-		bp->size += p_next->size;
-		bp->next = p_next->next;
-	} else if (p + p->size == bp) {
-		printf("lower boundary merge\n");
-		p->size += bp->size;
-		p->next = p_next;
-		p_next->prev = p;
-	}
-	
-	print_flist_head();
 }
 
+// This is a recommended helper function 
 void *acquire_more_heap(size_t nunits)
 {
-	printf("[DEBUG] acquire_more_heap\n");
-	size_t nbytes, npage_frames;		
-	header_t *p, *save;
-	
-	if (nunits < MIN_BLOCK_SIZE) 
-		nunits = MIN_BLOCK_SIZE;
-	npage_frames = div_ceil(nunits * sizeof(header_t), FOUR_KB);
-	nbytes = npage_frames * FOUR_KB;	
-	alloc_pages((virtual_addr *) wilderness, npage_frames);
-
-	p = (header_t *) wilderness;
-	p->size = nbytes / sizeof(header_t);
-	printf("%i\n", p->size);
-	p->prev = _flist_head->prev;
-	p->next = _flist_head;
-	_flist_head->prev->next = p;
-	_flist_head->prev = p;
-	
-	wilderness += nbytes;
-	return p;
 }
 
+// You should implement something to reset the heap
+// For debugging only (note the __)
 void __kheap_reset(void)
 {
-	_flist_head->next = _flist_head;
-	_flist_head->prev = _flist_head;
-	_flist_head->size = HEAP_START_SIZE;
-}
-
-void *get_flist_head(void)
-{
-	return (void *)_flist_head;
-}
-
-void print_flist_head(void)
-{
-	header_t *cur = _flist_head;
-	do {
-		printf("@%x[%n|%x] ", cur, cur->size, cur->next); 
-	} while ((cur = cur->next) != _flist_head);
-	printf("\n");
 }
--- kernel/test/kmalloc_test.c
+++ kernel/test/kmalloc_test.c
@@ -2,102 +2,11 @@
 #include <test/kmalloc_test.h>
 #include <test/unit.h>
 
-NEW_TEST_SUITE(kmallocTest, 9);
+NEW_TEST_SUITE(kmallocTest, 1);
 
-TEST(ExpectKmalloc0ByteRequestNull)
+TEST(ShouldNotPanic)
 {
-	EXPECT_EQ(NULL, kmalloc(0));
-}
-
-TEST(LowerBoundaryKmalloc)
-{
-	EXPECT_NQ(NULL, kmalloc(1));
-}
-
-TEST(HighBoundaryKmalloc)
-{
-	EXPECT_NQ(NULL, kmalloc(50000));
-}
-
-TEST(ConsecutiveAcquireMoreHeap)
-{
-	void *ptr_before = kmalloc(256);
-	void *ptr_mid = kmalloc(100);
-	void *ptr_after = kmalloc(1000);
-
-	EXPECT_NQ(NULL, ptr_before);
-	EXPECT_NQ(NULL, ptr_mid);
-	EXPECT_NQ(NULL, ptr_after);
-}
-
-TEST(ConsecutiveTinyKmalloc)
-{
-	void *ptr0 = kmalloc(1);
-	void *ptr1 = kmalloc(1);
-	void *ptr2 = kmalloc(1);
-	void *ptr3 = kmalloc(1);
-	void *ptr4 = kmalloc(1);
-	void *ptr5 = kmalloc(1);
-
-	EXPECT_NQ(NULL, ptr0);
-	EXPECT_NQ(NULL, ptr1);
-	EXPECT_NQ(NULL, ptr2);
-	EXPECT_NQ(NULL, ptr3);
-	EXPECT_NQ(NULL, ptr4);
-	EXPECT_NQ(NULL, ptr5);
-}
-
-TEST(ConsecutiveLargeKmalloc)
-{
-	void *ptr0 = kmalloc(4200);
-	void *ptr1 = kmalloc(4200);
-	void *ptr2 = kmalloc(4200);
-	void *ptr3 = kmalloc(4200);
-	void *ptr4 = kmalloc(4200);
-	void *ptr5 = kmalloc(4200);
-
-	EXPECT_NQ(NULL, ptr0);
-	EXPECT_NQ(NULL, ptr1);
-	EXPECT_NQ(NULL, ptr2);
-	EXPECT_NQ(NULL, ptr3);
-	EXPECT_NQ(NULL, ptr4);
-	EXPECT_NQ(NULL, ptr5);
-}
-
-TEST(Kfree)
-{
-	// Should not kernel panic
-	void *ptr0 = kmalloc(5000);
-	void *ptr1 = kmalloc(5000);
-	void *ptr2 = kmalloc(5000);
-	void *ptr3 = kmalloc(5000);
-	void *ptr4 = kmalloc(5000);
-	void *ptr5 = kmalloc(5000);
-	kfree(ptr4);
-	kfree(ptr2);
-	kfree(ptr0);
-	kfree(ptr5);
-	kfree(ptr3);
-	kfree(ptr1);
-}
-
-TEST(HeapReset)
-{
-	header_t *cur = get_flist_head();
-	__kheap_reset();
-	print_flist_head();	
-	EXPECT_EQ(cur, cur->prev);	
-	EXPECT_EQ(cur, cur->next);
-}
-
-TEST(FillandEmptyHeap)
-{
-	void *ptr0 = kmalloc(4000);
-	void *ptr1 = kmalloc(4000);
-	print_flist_head();	
-	kfree(ptr0);
-	kfree(ptr1);
-	print_flist_head();	
+	kmalloc(1);
 }
 
 END_SUITE();
